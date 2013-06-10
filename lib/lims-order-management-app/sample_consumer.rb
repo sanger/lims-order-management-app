16e6e84389744f80989ce1f24341e5876f59f97b
Loic <llh1@sanger.ac.uk>
2013-06-10 16:12:08 +0100

    sample consumer done

--- lib/lims-order-management-app/sample_consumer.rb
+++ lib/lims-order-management-app/sample_consumer.rb
@@ -2,10 +2,13 @@ require 'lims-busclient'
 require 'common'
 require 'lims-order-management-app/sample_json_decoder'
 require 'lims-order-management-app/order_creator'
+require 'lims-order-management-app/rule_matcher'
 
 module Lims::OrderManagementApp
   class SampleConsumer
     include Lims::BusClient::Consumer
+    include RuleMatcher
+    include SampleJsonDecoder
     include Virtus
     include Aequitas
 
@@ -13,12 +16,13 @@ module Lims::OrderManagementApp
     attribute :log, Object, :required => true, :writer => :private, :reader => :private
     attribute :order_creator, OrderCreator, :required => true, :writer => :private, :reader => :private
 
-    SampleNotPublished = Class.new(StandardError)
+    NoSamplePublished = Class.new(StandardError)
+
     SAMPLE_PUBLISHED_STATE = "published"
     EXPECTED_ROUTING_KEY_PATTERNS = [
       '*.*.sample.create', '*.*.sample.updatesample', 
       '*.*.bulkcreatesample.*', '*.*.bulkupdatesample.*' 
-    ].map { |k| Regexp.new(k.gsub(/\./, "\\.").gsub(/\*/, ".*")) }
+    ].map { |k| Regexp.new(k.gsub(/\./, "\\.").gsub(/\*/, "[^\.]*")) }
 
     def initialize(amqp_settings, api_settings)
       @queue_name = amqp_settings.delete("queue_name")
@@ -33,24 +37,44 @@ module Lims::OrderManagementApp
 
     private
 
-    def before_filter(sample)
-      raise SampleNotPublished if sample.state != SAMPLE_PUBLISHED_STATE
+    # @param [Array] samples
+    def before_filter!(samples)
+      samples.reject! do |resource|
+        resource[:sample].state != SAMPLE_PUBLISHED_STATE 
+      end
+      raise NoSamplePublished, "No samples with a published state found" if samples.empty?
     end
 
     def set_queue
       self.add_queue(queue_name) do |metadata, payload|
+        log.info("Message received with the routing key: #{metadata.routing_key}") 
         if expected_message?(metadata.routing_key)
-          begin
-            sample = sample_resource(payload) 
-            before_filter(sample)
-            pipeline = matching_rule(sample)
-            order_creator.execute(sample, pipeline)
-          rescue SampleNotPublished, NonMatchingRule
-           metadata.reject  
-          else
-            metadata.ack
-          end
+          log.debug("Processing message with routing key: '#{metadata.routing_key}' and payload: #{payload}") 
+          consume_message(metadata, payload)
+        else
+          metadata.reject
+          log.debug("Message rejected: unexpected message (routing key: #{metadata.routing_key})") 
         end
+      end
+    end
+
+    def consume_message(metadata, payload)
+      begin
+        samples = sample_resource(payload)
+        # TODO : before_filter!(samples)
+        # TODO: we assume hereby that all the samples 
+        # have the same sample_type and the same lysed option.
+        pipeline = matching_rule(samples.first[:sample])
+        order_creator.execute(samples, pipeline)
+      rescue NoSamplePublished, RuleMatcher::NonMatchingRule => e
+        metadata.reject
+        log.error("Sample message rejected: #{e}")
+      rescue OrderCreator::TubeNotFound => e
+        metadata.reject(:requeue => true)
+        log.error("Sample message requeued: #{e}")
+      else
+        #metadata.ack
+        log.info("Sample message processed and acknowledged. Order created.")
       end
     end
 
