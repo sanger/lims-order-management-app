c17246ec57b17367ed7f0d4c978595393eb3a574
Loic <llh1@sanger.ac.uk>
2013-06-10 16:11:32 +0100

    Order creator class

--- lib/lims-order-management-app/order_creator.rb
+++ lib/lims-order-management-app/order_creator.rb
@@ -9,13 +9,71 @@ module Lims::OrderManagementApp
     include Aequitas
     include Helpers::API
 
+    TubeNotFound = Class.new(StandardError)
+    INPUT_TUBE_ROLE = "samples.extraction.manual.dna_and_rna.input_tube_nap"
+
+    # @param [Hash] api_settings
     def initialize(api_settings)
-      @root_url = api_settings["url"]      
-      initialize_api
+      url = api_settings["url"]      
+      initialize_api(url)
     end
 
-    def execute(sample, pipeline)
+    # @param [Array] samples
+    # @param [String] pipeline
+    def execute(samples, pipeline)
+      sample_uuids = samples.inject([]) { |m,e| m << e[:uuid] }
+      tube_uuids = tubes_by_sample_uuids(sample_uuids)
+      order_parameters = generate_order_parameters(tube_uuids, pipeline)
+      post_order(order_parameters)
+    end
 
+    private
+
+    # @param [String] sample_uuids
+    # @return [Array]
+    # Return an array of tube uuids which contain the samples.
+    def tubes_by_sample_uuids(sample_uuids)
+      parameters = {:search => {
+        :description => "search for tubes by sample uuids",
+        :model => "tube",
+        :criteria => {
+          :sample => {:uuid => sample_uuids}
+        }
+      }}
+      search = post(url_for(:searches, :create), parameters)
+      result_url = search["search"]["actions"]["first"]
+      result = get(result_url)
+
+      sample_uuids_in_tubes = [].tap do |uuids|
+        result["tubes"].each do |tube|
+          tube["aliquots"].each do |aliquot|
+            uuids << aliquot["sample"]["uuid"]
+          end
+        end
+      end.uniq
+
+      orphan_sample_uuids = sample_uuids - sample_uuids_in_tubes
+      raise TubeNotFound, "Can't find a tube containing the samples #{orphan_sample_uuids.to_s}" unless orphan_sample_uuids.empty? 
+      result["tubes"].inject([]) { |m,e| m << e["uuid"]}
+    end
+
+    # @param [Array] tube_uuids
+    # @param [String] pipeline
+    # @return [Hash]
+    def generate_order_parameters(tube_uuids, pipeline)
+      {:order => {}.tap do |p|
+        p[:user_uuid] = "66666666-2222-4444-9999-000000000000"
+        p[:study_uuid] = "55555555-2222-3333-6666-777777777777"
+        p[:pipeline] = pipeline
+        p[:cost_code] = "cost code"
+        p[:sources] = {INPUT_TUBE_ROLE => tube_uuids}
+      end
+      }
+    end
+
+    # @param [Hash] order_parameters
+    def post_order(order_parameters)
+      post(url_for(:orders, :create), order_parameters)
     end
   end
 end
