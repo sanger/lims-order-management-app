ceda5e9feac3347e6d0eb3d08136d08d90072ca9
Loic <llh1@sanger.ac.uk>
2013-06-10 16:11:10 +0100

    Refactor api helper

--- lib/lims-order-management-app/helpers/api.rb
+++ lib/lims-order-management-app/helpers/api.rb
@@ -9,28 +9,29 @@ module Lims::OrderManagementApp
 
       module Request
         def get(url)
-          RestClient.get(url, HEADERS)
+          response = RestClient.get(url, HEADERS)
+          JSON.parse(response)
         end
 
         def post(url, parameters)
-          RestClient.post(url, parameters.to_json, HEADERS)
+          response = RestClient.post(url, parameters.to_json, HEADERS)
+          JSON.parse(response)
         end
       end
       include Request
 
       def self.included(klass)
         klass.class_eval do
-          attribute :root_url, String, :required => true, :writer => :private, :reader => :private
           attribute :root, String, :required => true, :writer => :private, :reader => :private
         end
       end
 
-      def initialize_api
-        root = JSON.parse(get(root_url))        
+      def initialize_api(root_url)
+        @root = get(root_url)        
       end
 
       def url_for(model, action)
-        root[model.to_s]["actions"][action.to_s]
+        @root[model.to_s]["actions"][action.to_s]
       end
     end
   end
