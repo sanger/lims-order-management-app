ac47a7aa4237777129493d93baaabdd348932ba3
Matthew Denner <md12@sanger.ac.uk>
2013-06-20 15:44:58 +0100

    Put env in the wrong place

--- script/start_order_creator.rb
+++ script/start_order_creator.rb
@@ -1,11 +1,11 @@
-env = ENV["LIMS_ORDER_MANAGEMENT_APP_ENV"] or raise "LIMS_ORDER_MANAGEMENT_APP_ENV is not set in the environment"
-
 require 'yaml'
 require 'lims-order-management-app'
 require 'logging'
 
 module Lims
   module OrderManagementApp
+    env = ENV["LIMS_ORDER_MANAGEMENT_APP_ENV"] or raise "LIMS_ORDER_MANAGEMENT_APP_ENV is not set in the environment"
+
     amqp_settings = YAML.load_file(File.join('config','amqp.yml'))[env]
     api_settings = YAML.load_file(File.join('config','api.yml'))[env]
     order_settings = YAML.load_file(File.join('config','order.yml'))[env]
