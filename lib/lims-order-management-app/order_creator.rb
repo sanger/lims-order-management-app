7e9cb0ee17d69beee954e9a5cd402f3dff181a5b
Loic <llh1@sanger.ac.uk>
2014-01-30 17:08:35 +0000

    Rewrite rule matcher and order creator as we know in the extraction process field the container uuids of the sample

--- lib/lims-order-management-app/order_creator.rb
+++ lib/lims-order-management-app/order_creator.rb
@@ -27,82 +27,30 @@ module Lims::OrderManagementApp
 
     # @param [Array] samples
     def create!(samples)
-      sample_uuids_to_roles = Hash[samples.map { |s| [s[:uuid], matching_rule(s[:sample])] }]
-      container_uuids_to_roles = containers_for_samples(sample_uuids_to_roles)
-      order_parameters = order_creation_parameters(container_uuids_to_roles)
+      container_roles = container_roles(samples)
+      order_parameters = order_parameters(container_roles)
       post_order(order_parameters)
     end
 
     private
 
-    # @param [Hash] sample_uuids_to_roles
-    # @return [Hash]
-    def containers_for_samples(sample_uuids_to_roles)
-      model, result = nil, nil
-      [:tube, :filter_paper, :plate].each do |m|
-        result = search_for(m, sample_uuids_to_roles.keys)
-        unless result.nil? || result.empty? 
-          model = m
-          break
-        end
-      end
-
-      first_sample_in_each_container = []
-      sample_uuids_in_containers = [].tap do |uuids|
-        result.each do |container|
-          first_sample_uuid_added = false
-          foreach_aliquot_of(model, container) do |aliquot|
-            sample_uuid = aliquot["sample"]["uuid"]
-            uuids << sample_uuid
-            unless first_sample_uuid_added
-              first_sample_in_each_container << sample_uuid
-              first_sample_uuid_added = true
-            end
+    # @param [Array] samples
+    # @return [Hash] 
+    # @example: {"11111111-2222-3333-4444-555555555555" => "role"}
+    def container_roles(samples)
+      {}.tap do |result|
+        samples.each do |sample_data|
+          matching_rule(sample_data[:sample]).each do |container_uuid, role|
+            result[container_uuid] = role unless result.has_key?(container_uuid)
           end
         end
-      end.uniq
-
-      orphan_sample_uuids = sample_uuids_to_roles.keys - sample_uuids_in_containers
-      raise SampleContainerNotFound, "Can't find a resource containing the samples #{orphan_sample_uuids.to_s}" unless orphan_sample_uuids.empty? 
-
-      Hash[result.map { |t| [t['uuid'], sample_uuids_to_roles[first_sample_in_each_container.shift]] }]
-    end
-
-    # @param [String, Symbol] model
-    # @param [Array] sample_uuids
-    # @return [Array]
-    def search_for(model, sample_uuids)
-      model = model.to_s
-      parameters = {:search => {
-        :description => "search for #{model} by sample uuids",
-        :model => model,
-        :criteria => {
-          :sample => {:uuid => sample_uuids}
-        }
-      }}
-      search = post(url_for("laboratory-searches", :create), parameters)
-      result_url = search["search"]["actions"]["first"]
-      result = get(result_url)
-      result["#{model}s"]
-    end
-
-    # @param [String, Symbol] model
-    # @param [Hash] resource_hash
-    # @param [Block] block
-    def foreach_aliquot_of(model, resource_hash, &block)
-      case model.to_sym
-      when :tube, :filter_paper then resource_hash["aliquots"].each { |aliquot| block.call(aliquot) } 
-      else
-        resource_hash["wells"].each do |_, aliquots|
-           aliquots.each { |aliquot| block.call(aliquot) }
-        end
       end
     end
 
-    # @param [Map] tube UUID to role in order
+    # @param [Hash] container_roles
     # @return [Hash]
-    def order_creation_parameters(container_uuids_to_roles)
-      roles_to_container_uuids = container_uuids_to_roles.inject(Hash.new { |h,k| h[k] = [] }) do |m,(k,v)|
+    def order_parameters(container_roles)
+      roles_to_container_uuids = container_roles.inject(Hash.new { |h,k| h[k] = [] }) do |m,(k,v)|
         m[v] << k
         m
       end
--- lib/lims-order-management-app/rule_matcher.rb
+++ lib/lims-order-management-app/rule_matcher.rb
@@ -2,26 +2,45 @@ module Lims::OrderManagementApp
   module RuleMatcher
    
     NoMatchingRule = Class.new(StandardError)
+    SampleExtractionProcessField = "cellular_material.extraction_process"
 
     def initialize_rules(rule_settings)
       @ruleset = rule_settings["rules"]
     end
 
     # @param [Lims::ManagementApp::Sample] sample
-    # @return [String]
+    # @return [Hash]
+    # @example returned value: {"11111111-2222-3333-4444-555555555555" => "samples.extraction.manual_dna_and_rna.input_tube_nap"}
+    # @raise [NoMatchingRule]
     def matching_rule(sample)
+      item_roles = {}
       @ruleset.each do |ruleset_items|
         ruleset_items.each do |rules|
           role = rules.keys.first
           rule_items = rules.values[0]
-          return role if rule_items.all? do |key, value|
-            value_from_sample = key.split('.').inject(sample) { |v,f| v && v[f.to_sym] }
-            value_from_sample == value
+          rule_extraction_process = rule_items[SampleExtractionProcessField]
+
+          # For all the other rules than extraction process (if any)
+          valid = rule_items.reject { |r,_| r == SampleExtractionProcessField }.all? do |rule_key, rule_value|
+            sample_value = rule_key.split('.').inject(sample) do |sample, field|
+              sample && sample[field.to_sym]
+            end
+          end
+          next unless valid
+
+          # Extraction process rule 
+          sample[:cellular_material][:extraction_process].each do |sample_extraction_process, container_uuids|
+            if sample_extraction_process == rule_extraction_process 
+              container_uuids.each do |container_uuid|
+                item_roles[container_uuid] = role
+              end
+            end
           end
         end
       end
 
-      raise NoMatchingRule
+      raise NoMatchingRule if item_roles.empty?
+      item_roles
     end
   end
 end
